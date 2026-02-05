e7c1950323dbb7b3bbb6becd8a4a10391a93ed5f
JoelNovoAU <playjoel162@gmail.com>
2026-02-05 17:12:10 +0100

    modificacion

--- GimnasioOrto/apigimnasio/src/inngest/index.js
+++
@@ -1,8 +0,0 @@
-import { serve } from "inngest/express";
-import { inngest } from "./cliente.js";
-import { revisionActividadCadaMinuto } from "./revisionActividad.js";
-
-export default serve({
-  client: inngest,
-  functions: [revisionActividadCadaMinuto],
-});
